352fcdd7891f6e1f0d361550b24f20c9284675f4
Caroline Robinson <CRobinson@mlri.org>
2020-10-06 06:22:56 -0400

    what repository will this go to after transferring ownership to MLH, using MAVirtualcourt show_id.js

--- docassemble/MLHDevGlobalFiles/data/static/show_id.js
+++ docassemble/MLHDevGlobalFiles/data/static/show_id.js
@@ -1,5 +1,11 @@
 $(document).on('daPageLoad', function(){
-  if (daQuestionID && daQuestionID['id']){
-    $("#daQuestionIdPart").html(daQuestionID['id']);
-  }
+  // I believe this is async. We won't rely on `await` for now -
+  // it's less browser compatible.
+  get_interview_variables( function( data ){
+    var verText = ''
+    if ( data.success && data.variables.package_version_number ) {
+      verText = ', v' + data.variables.package_version_number;
+    }
+    $( "#qId" ).html( "<code>Page id: " + daQuestionID['id'] + verText + "</code>" );
+  });
 });
